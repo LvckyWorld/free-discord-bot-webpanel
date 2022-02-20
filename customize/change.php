75cc1513162b04aee26d7e40eab47ddd571232f4
LvckyAPI | Iven <stossenhd@gmail.com>
2022-02-20 23:12:02 +0100

    add todo instruction

--- customize/change.php
+++ customize/change.php
@@ -1,1 +1,8 @@
 <!-- apply changes from index -->
+<!-- You have to do this: 
+     - add the functions
+     - if avatarurl == null/undefined && username == null/undefined -> return error
+     - if avatarurl == null/undefined but username has a value -> update username
+     - if username == null/undefined but avatarurl has a value -> update avatarurl
+     - if both have a value -> update both
+-->
